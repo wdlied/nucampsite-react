61955ff9ccd1a590c453f984325708ae484fee71
Will Lied <wdlied@gmail.com>
2020-01-04 12:32:29 -0500

    workshop2

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import CampsiteInfo from './CampsiteInfoComponent';
 
 class Directory extends Component {
     constructor(props) {
@@ -11,21 +12,6 @@ class Directory extends Component {
 
     onCampsiteSelect(campsite) {
         this.setState({selectedCampsite: campsite});
-    }
-
-    renderSelectedCampsite(campsite) {
-        if (campsite) {
-            return (
-                <Card>
-                    <CardImg top src={campsite.image} alt={campsite.name} />
-                    <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        }
-        return <div />;
     }
 
     render() {
@@ -48,33 +34,14 @@ class Directory extends Component {
                 <div className="row">
                     {directory}
                 </div>
-                <div className="row">
+                {/* <div className="row">
                     <div className="col-md-5 m-1">
                         {this.renderSelectedCampsite(this.state.selectedCampsite)}
                     </div>
-                </div>
-                <ExampleParentComponent />
+                </div> */}
+                <CampsiteInfo campsite={this.state.selectedCampsite}/>
             </div>
         );
-    }
-}
-
-class ExampleParentComponent extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            number: 333
-        }
-    }
-
-    render() {
-        return <ExampleChildComponent number={this.state.number} greeting="Hello World"/>
-    }
-}
-
-class ExampleChildComponent extends ExampleParentComponent {
-    render() {
-        return <div>{this.state.number} {this.props.greeting}</div>
     }
 }
 
