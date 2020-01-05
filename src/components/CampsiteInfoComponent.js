44280c2b2e851563c7522d46055a350c1242eda0
Will Lied <wdlied@gmail.com>
2020-01-05 10:19:22 -0500

    Presentational and Container Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class CampsiteInfo extends Component {
-    constructor(props) {
-        super(props);
-    }
 
     renderCampsite(campsite) {
         return (
@@ -47,9 +44,11 @@ class CampsiteInfo extends Component {
     render() {
         if (this.props.campsite) {
             return ( 
-                <div className="row">
-                    {this.renderCampsite(this.props.campsite)}
-                    {this.renderComments(this.props.campsite.comments)}
+                <div className="container">
+                    <div className="row">
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
                 </div>
             );
         } else {
