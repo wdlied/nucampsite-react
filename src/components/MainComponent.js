bf439cef1d7be9cb2b0ce0960eea93f4ff3a49bc
Will Lied <wdlied@gmail.com>
2020-01-19 16:15:11 -0500

    Redux Actions

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,6 +8,7 @@ import About from './AboutComponent';
 import Contact from './ContactComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { addComment} from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
   return {
@@ -17,6 +18,10 @@ const mapStateToProps = state => {
     promotions: state.promotions
   }
 }
+
+const mapDispatchToProps = {
+  addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text))
+};
 
 class Main extends Component {
 
@@ -33,7 +38,13 @@ class Main extends Component {
 
       const CampsiteWithId = ({match}) => {
         return (
-          <CampsiteInfo campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]} comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)} />
+          <CampsiteInfo 
+            campsite={this.props.campsites.filter(campsite => campsite.id === 
+              +match.params.campsiteId)[0]} 
+            comments={this.props.comments.filter(comment => comment.campsiteId === 
+              +match.params.campsiteId)} 
+            addComment={this.props.addComment}
+          />
         );
       }
 
@@ -54,4 +65,4 @@ class Main extends Component {
   }
 }
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
