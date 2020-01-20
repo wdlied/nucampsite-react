84a2d8c5fb3ec749ce7dbb26c74587f0e030b52e
Will Lied <wdlied@gmail.com>
2020-01-19 20:45:03 -0500

    Redux Thunk

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,17 +1,22 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+// import { thunk } from "redux-thunk";
+import thunk from 'redux-thunk';
+import logger  from 'redux-logger';
 import { Campsites } from './campsites';
 import { Comments} from './comments';
 import { Partners } from './partners';
 import { Promotions} from './promotions';
 
 export const ConfigureStore = () => {
+
     const store = createStore(
         combineReducers({
             campsites: Campsites,
             comments: Comments,
             partners: Partners,
             promotions: Promotions
-        })
+        }), 
+        applyMiddleware( thunk, logger )
     );
 
     return store;
